3c5ae9b87f508dae3c8bf8110d88adb050a044ef
Takahiro <tkhr_621@docomo.ne.jp>
2022-05-10 01:03:38 +0900

    google 認証実装

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  // プロバイダーを構成
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: 'secret',
+})
